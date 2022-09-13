99d139fe7659430a04050dc90737487400be16c4
Nicolas Alvez <nicoalvez99@gmail.com>
2022-09-13 15:11:54 -0300

    Cambios

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let boton = document.getElementById('btnEnviar');
+
+boton.addEventListener("click", () =>{
+    let sueldo = document.getElementById('txtSueldo').value;
+    document.write(sueldo);
+
+});
